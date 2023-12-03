0fcbb6c82cbf207562836b230b0168e100b0d017
Kshitij Saraogi <kshitij.saraogi@gmail.com>
2023-12-03 22:20:27 +0100

    feat(day03): implement part 2

--- day03/gears.clj
+++ day03/gears.clj
@@ -2,119 +2,7 @@
 (ns gears
   (:require [clojure.string]))
 
-;; (defn get-entry [schematic [i j]]
-;;   (nth (nth schematic i) j))
-
-;; (defn is-adjacent-to-middle? [schematic i j]
-;;   (or (is-special-symbol? (get-entry schematic [(dec i) (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [(dec i) j]))
-;;       (is-special-symbol? (get-entry schematic [(dec i) (inc j)]))
-;;       (is-special-symbol? (get-entry schematic [i (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [i (inc j)]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) j]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) (inc j)]))))
-
-;; (defn is-adjacent-top-left? [schematic]
-;;   (let [m (count schematic)
-;;         n (count (first schematic))]
-;;   (or (and (> n 1) (is-special-symbol? (get-entry schematic [0 1])))
-;;       (and (> m 1) (is-special-symbol? (get-entry schematic [1 0])))
-;;       (and (> m 1) (> n 1) (is-special-symbol? (get-entry schematic [1 1]))))))
-
-;; (defn is-adjacent-top-right? [schematic]
-;;   (let [n (count (first schematic))
-;;         i 0
-;;         j (dec n)]
-;;   (or (is-special-symbol? (get-entry schematic [i (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) j])))))
-
-;; (defn is-adjacent-bottom-left? [schematic]
-;;   (let [m (count schematic)
-;;         n (count (first schematic))
-;;         i (dec m)
-;;         j 0]
-;;     (or (is-special-symbol? (get-entry schematic [(dec i) j]))
-;;         (is-special-symbol? (get-entry schematic [(dec i) (inc j)]))
-;;         (is-special-symbol? (get-entry schematic [i (inc j)])))))
-
-;; (defn is-adjacent-bottom-right? [schematic]
-;;   (let [m (count schematic)
-;;         n (count (first schematic))
-;;         i (dec m)
-;;         j (dec n)]
-;;     (or (is-special-symbol? (get-entry schematic [i (dec j)]))
-;;         (is-special-symbol? (get-entry schematic [(dec i) (dec j)]))
-;;         (is-special-symbol? (get-entry schematic [(dec i) j])))))
-
-;; (defn is-adjacent-top? [schematic j]
-;;   (or (is-special-symbol? (get-entry schematic [0 (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [0 (inc j)]))
-;;       (is-special-symbol? (get-entry schematic [1 (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [1 j]))
-;;       (is-special-symbol? (get-entry schematic [1 (inc j)]))))
-
-;; (defn is-adjacent-bottom? [schematic j]
-;;   (let [m (count schematic)
-;;         i (dec m)]
-;;   (or (is-special-symbol? (get-entry schematic [(dec i) (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [(dec i) j]))
-;;       (is-special-symbol? (get-entry schematic [(dec i) (inc j)]))
-;;       (is-special-symbol? (get-entry schematic [i (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [i (inc j)])))))
-
-;; (defn is-adjacent-to-left? [schematic i]
-;;   (let [m (count schematic)
-;;         n (count (first schematic))
-;;         j 0]
-;;   (or (is-special-symbol? (get-entry schematic [(dec i) j]))
-;;       (is-special-symbol? (get-entry schematic [(dec i) (inc j)]))
-;;       (is-special-symbol? (get-entry schematic [i (inc j)]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) j]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) (inc j)])))))
-
-;; (defn is-adjacent-to-right? [schematic i]
-;;   (let [m (count schematic)
-;;         n (count (first schematic))
-;;         j (dec n)]
-;;   (or (is-special-symbol? (get-entry schematic [(dec i) (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [(dec i) j]))
-;;       (is-special-symbol? (get-entry schematic [i (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) (dec j)]))
-;;       (is-special-symbol? (get-entry schematic [(inc i) j])))))
-
-
-;; (defn is-adjacent? [schematic i j]
-;;   (let [m (count schematic)
-;;         n (count (first schematic))]
-;;     (cond
-;;       (and (= i 0) (= j 0)) (is-adjacent-top-left? schematic)
-;;       (and (= i 0) (= j (dec n))) (is-adjacent-top-right? schematic)
-;;       (and (= i (dec m)) (= j 0)) (is-adjacent-bottom-left? schematic)
-;;       (and (= i (dec m)) (= j (dec n))) (is-adjacent-bottom-right? schematic)
-;;       (= i 0) (is-adjacent-top? schematic j)
-;;       (= i (dec m)) (is-adjacent-bottom? schematic j)
-;;       (= j 0) (is-adjacent-to-left? schematic i)
-;;       (= j (dec n)) (is-adjacent-to-right? schematic i) 
-;;       :else (is-adjacent-to-middle? schematic i j))))
-
-;; (defn is-adjacent-to-special-symbol?
-;;   [schematic i j n]
-;;   (is-adjacent? schematic i j))
-
-
-;; Locate the numbers in the schematics.
-;; Filter out the numbers which are not adjacent to special symbols.
-;; (defn find-numbers [schematic]
-;;   (doseq [[i row] (map-indexed vector schematic)]
-;;      (doseq [[j col] (map-indexed vector row)]
-;;        {:i i
-;;         :j j
-;;         :val col
-;;         :is-adjacent (is-adjacent-to-special-symbol? schematic i j col)})))
-
-
+;; Sample input from the problem statement.
 (def input "467..114..
 ...*......
 ..35..633.
@@ -128,62 +16,123 @@
 
 ;; Parsing the input
 (defn build-number-line [i line]
-    (let [matcher (re-matcher #"\d+" line)]
-      (loop [matches []]
-        (if (.find matcher)
-          (recur (conj matches {
-                                :i i
-                                :j (.start matcher)
-                                :value (Integer/parseInt (.group matcher))
-                                :len (count (.group matcher))
-                                }))
-          matches))))
-
-(defn build-special-symbol-line [i line]
-  (let [matcher (re-matcher #"[^\d\.]" line)] ;; Anything but a digit or a dot.
+  ;; Using regex to find all the numbers in a given line.
+  ;; The regex is \d+ which means one or more digits.
+  ;; It returns a collection of maps of numbers with their indices and values.
+  (let [matcher (re-matcher #"\d+" line)]
     (loop [matches []]
       (if (.find matcher)
         (recur (conj matches {:i i
-                              :j (.start matcher)}))
+                              :j (.start matcher)
+                              :value (Integer/parseInt (.group matcher))
+                              :len (count (.group matcher))}))
         matches))))
 
-(defn build-numbers [text]
+(defn find-numbers [text]
+  ;; Splitting the text into lines, then mapping over the lines to build the numbers.
+  ;; Then flattening the result and converting it to a vector.
   (->> text
        (clojure.string/split-lines)
        (map-indexed build-number-line)
        (flatten)
        (vec)))
 
+(defn build-special-symbol-line [i line]
+  ;; Using regex to find all the special symbols in a given line.
+  ;; The regex is [^\d\.] which means anything but a digit or a dot.
+  ;; It returns a collection of maps of special symbols with their indices.
+  (let [matcher (re-matcher #"[^\d\.]" line)]
+    (loop [matches []]
+      (if (.find matcher)
+        (recur (conj matches {:i i
+                              :j (.start matcher)}))
+        matches))))
+
 (defn build-special-symbols [text]
+  ;; Splitting the text into lines, then mapping over the lines to build the special symbols.
+  ;; Then flattening the result and converting it to a vector. 
   (->> text
        (clojure.string/split-lines)
        (map-indexed build-special-symbol-line)
        (flatten)
        (vec)))
 
-(defn is-adjacent-special-symbol? 
-  [i start end si sj]
-  (and (>= sj (dec start))
-       (<= sj (inc end))
-       (>= si (dec i))
-       (<= si (inc i))))
-
-(defn adjacent-symbol? [symbols, num]
+;; Logic
+(defn neighbour?
+  ;; Given a number and a special symbol, check if the special symbol is adjacent to the number.
+  [num symbol]
   (let [i (:i num)
         start (:j num)
-        end (+ start (- (:len num) 1))]
-    (filter #(is-adjacent-special-symbol? i start end (:i %) (:j %)) symbols)))
+        end (+ start (- (:len num) 1))
+        si (:i symbol)
+        sj (:j symbol)]
+    (and (>= sj (dec start))
+         (<= sj (inc end))
+         (>= si (dec i))
+         (<= si (inc i)))))
 
-(defn adjacent-nums [nums symbols]
+
+(defn neighbour-symbols 
+  ;; Given a collection of special symbols and a number, return the special symbols adjacent to the number. 
+  [symbols num] 
+  (filter #(neighbour? num %) symbols))
+
+(defn neighbours
+  ;; Given a collection of numbers and a collection of special symbols, return the sum of the numbers adjacent to the special symbols.
+  [nums symbols]
   (->> nums 
-       (map #(vector % (adjacent-symbol? symbols %))) 
-       (filter #(not (empty? (second %))))
-        (map first)
+       (map #(vector % (neighbour-symbols symbols %))) 
+       (filter #(not (empty? (second %))))))
+
+(defn nums-neighbouring-special-symbols
+  ;; Given a collection of numbers and a collection of special symbols, return the numbers adjacent to the special symbols.
+  [nums symbols]
+  (->> (neighbours nums symbols)
+       (map first)
        (map :value)))
 
 (defn solve-engine
-    [schematic]
-    (reduce + (adjacent-nums (build-numbers schematic) (build-special-symbols schematic))))
+  ;; Given a schematic, return the sum of the numbers adjacent to the special symbols. 
+  [schematic]
+  (reduce + (nums-neighbouring-special-symbols (find-numbers schematic) (build-special-symbols schematic))))
 
 (defn solve1 [path]
+  ;; Solves the first part of the problem.
   (solve-engine (slurp path)))
+
+;; Part 2
+(defn build-asterisks [i line]
+  ;; Using regex to find all the asterisks (*) in a given line.
+  (let [matcher (re-matcher #"[*]" line)] ;; Anything but a digit or a dot.
+    (loop [matches []]
+      (if (.find matcher)
+        (recur (conj matches {:i i
+                              :j (.start matcher)}))
+        matches))))
+
+(defn find-asterisks [text]
+  ;; Splitting the text into lines, then mapping over the lines to build the asterisks.
+  ;; Then flattening the result and converting it to a vector.
+  (->> text
+       (clojure.string/split-lines)
+       (map-indexed build-asterisks)
+       (flatten)
+       (vec)))
+
+(defn gear? [nums]
+  (= (count nums) 2))
+
+(defn gear-ratios [nums asterisks]
+  ;; Given a collection of numbers and a collection of asterisks, return the product of the gear ratios.
+  (->> (neighbours nums asterisks)
+       (filter #(gear? (second %))) 
+       (map second)
+       (map #(apply * %))))
+
+(defn compute-gears-ratios [schematic]
+  ;; Given a schematic, return the sum of all the gear ratios.
+  (reduce + (gear-ratios (find-numbers schematic) (find-asterisks schematic))))
+
+(defn solve2 [path]
+  ;; Solves the second part of the problem.
+  (compute-gears-ratios (slurp path)))
