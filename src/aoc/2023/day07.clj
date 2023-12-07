d0bdc74884ccbd1399cd96afe45336435274228f
Kshitij Saraogi <kshitij.saraogi@gmail.com>
2023-12-07 23:13:47 +0100

    feat(day07): refactor with tests

--- src/aoc/2023/day07.clj
+++ src/aoc/2023/day07.clj
@@ -1,83 +1,210 @@
 (ns aoc.2023.day07
   (:require [clojure.string]))
 
-(def rank-map {:A 14 :K 13 :Q 12 :J 1 :T 10 :9 9 :8 8 :7 7 :6 6 :5 5 :4 4 :3 3 :2 2})
+;; Map to store the strength of each card.
+;; The values are arbitrary, but the order is important.
+(def card-to-strength {:A 14 
+                       :K 13 
+                       :Q 12 
+                       :J 11 
+                       :T 10 
+                       :9 9 
+                       :8 8 
+                       :7 7 
+                       :6 6
+                       :5 5
+                       :4 4
+                       :3 3
+                       :2 2})
 
-(def type-map {:five-of-a-kind 9 :four-of-a-kind 8 :full-house 7 :three-of-a-kind 6 :two-pair 5 :one-pair 4 :high-card 3})
+;; Map to store the strength of each card, including the joker.
+;; The values are arbitrary, but the order is important.
+(def card-to-strength-with-joker (assoc card-to-strength :J 1))
 
-(defn string-to-character-frequencies [s]
-  (frequencies s))
+;; Map to store the strength of each hand type.
+;; The values are arbitrary, but the order is important.
+(def hand-type-to-strength {:five-of-a-kind 9
+                            :four-of-a-kind 8
+                            :full-house 7
+                            :three-of-a-kind 6
+                            :two-pair 5
+                            :one-pair 4
+                            :high-card 3})
+
+;; ==================
+;; Hand type logic
+;; ==================
+
+(defn build-char-map-without-jokers [hand] (frequencies hand))
 
 (defn consolidate-jokers [frequencies]
   (let [joker-count (get frequencies \J 0)]
-    (if (or (zero? joker-count) (= joker-count 5)) (vals frequencies)
+    (if (or (zero? joker-count) (= joker-count 5)) frequencies
         (let [freqs-without-joker (dissoc frequencies \J)
               rank-to-be-replaced (key (apply max-key val freqs-without-joker))]
-          (vals (assoc freqs-without-joker rank-to-be-replaced
+          (assoc freqs-without-joker rank-to-be-replaced
                        (+ (get freqs-without-joker rank-to-be-replaced 0)
-                          joker-count)))))))
-    
-(defn find-vals [hand] 
-  (consolidate-jokers (string-to-character-frequencies hand)))
+                          joker-count))))))
 
-(defn get-hand-type [hand]
-    (let [ values (find-vals hand)
-            sorted-values (sort values)
-            sorted-values-reversed (reverse sorted-values)] 
-      (cond
-        (= sorted-values-reversed [5]) :five-of-a-kind
-        (= sorted-values-reversed [4 1]) :four-of-a-kind
-        (= sorted-values-reversed [3 2]) :full-house
-        (= sorted-values-reversed [3 1 1]) :three-of-a-kind 
-        (= sorted-values-reversed [2 2 1]) :two-pair 
-        (= sorted-values-reversed [2 1 1 1]) :one-pair
-        :else :high-card)))
 
-(defn compare-ranks [rank1 rank2]
-  (compare (rank-map (keyword (str rank1))) (rank-map (keyword (str rank2)))))
+(defn build-char-map-with-jokers [hand]
+  (consolidate-jokers (build-char-map-without-jokers hand)))
 
-(defn non-zero? [x]
-  (not (= x 0)))
+(defn build-hand-config [builder hand] 
+  (reverse (sort (vals (builder hand)))))
 
-(defn compare-hands [hand1 hand2]
-  (let [res (first (filter #(not= % 0) (map compare-ranks hand1 hand2)))]
+(defn get-builder [joker]
+  (if joker
+    build-char-map-with-jokers
+    build-char-map-without-jokers))
+
+(defn get-hand-type [hand config-builder]
+  (let [config (build-hand-config config-builder hand)] 
     (cond
-      (nil? res) nil
+      (= config [5]) :five-of-a-kind
+      (= config [4 1]) :four-of-a-kind
+      (= config [3 2]) :full-house
+      (= config [3 1 1]) :three-of-a-kind
+      (= config [2 2 1]) :two-pair
+      (= config [2 1 1 1]) :one-pair
+      :else :high-card)))
+
+;; ==================
+;; Hand comparison logic
+;; ==================
+
+(defn compare-cards-char-by-char
+  "Returns 1 if c1 is stronger, -1 if c2 is stronger, and 0 if they are equal or the cards are incomparable.
+   
+   It implements java.util.Comparator.compare."
+  [c1 c2]
+  (compare (card-to-strength (keyword (str c1))) 
+           (card-to-strength (keyword (str c2)))))
+
+(defn compare-cards-char-by-char-with-joker
+  "Returns 1 if c1 is stronger, -1 if c2 is stronger, and 0 if they are equal or the cards are incomparable.
+   
+   It implements java.util.Comparator.compare."
+  [c1 c2]
+  (compare (card-to-strength-with-joker (keyword (str c1)))
+           (card-to-strength-with-joker (keyword (str c2)))))
+
+
+(defn compare-hands-second-order 
+  "Returns 1 if hand1 is stronger, -1 if hand2 is stronger, and 0 if they are equal or the hands are incomparable.
+   It implements java.util.Comparator.compare."
+  [h1 h2] 
+  (let [res (first (filter #(not= % 0) (map compare-cards-char-by-char 
+                                            h1 
+                                            h2)))]
+    (cond
+      (nil? res) 0
       (> res 0) 1
       (< res 0) -1
-      :else nil)))
+      :else 0)))
 
-(defn sort-hands [h1 h2]
-  (let [hand1 (h1 :hand)
-        hand2 (h2 :hand)
-        hand1-type (type-map (get-hand-type hand1))
-        hand2-type (type-map (get-hand-type hand2))] 
+(defn compare-hands-second-order-with-joker
+  "Returns 1 if hand1 is stronger, -1 if hand2 is stronger, and 0 if they are equal or the hands are incomparable.
+   It implements java.util.Comparator.compare."
+  [h1 h2]
+  (let [res (first (filter #(not= % 0) (map compare-cards-char-by-char-with-joker
+                                            h1
+                                            h2)))]
     (cond
-      (> hand1-type hand2-type) 1
-      (< hand1-type hand2-type) -1
-      :else (compare-hands hand1 hand2))))
+      (nil? res) 0
+      (> res 0) 1
+      (< res 0) -1
+      :else 0)))
 
+(defn compare-hands-first-order
+  "Returns 1 if hand1 is stronger, -1 if hand2 is stronger, and 0 if they are equal or the hands are incomparable.
+   
+   It implements java.util.Comparator.compare."
+  [h1 h2]
+  (let [h1-strength (hand-type-to-strength h1)
+        h2-strength (hand-type-to-strength h2)] 
+    (compare h1-strength 
+             h2-strength)))
+
+(defn compare-hands 
+  "Returns 1 if hand1 is stronger, -1 if hand2 is stronger, and 0 if they are equal or the hands are incomparable.
+   
+   First-ordering rule: hand type strength.
+   Second-ordering rule: compare hands sequentially.
+   
+   It implements java.util.Comparator.compare."
+  [h1 h2]
+  (let [res-first-order (compare-hands-first-order (:type h1) (:type h2))]
+    (if (zero? res-first-order)
+      (compare-hands-second-order (:hand h1) (:hand h2))
+      res-first-order)))
+
+(defn compare-hands-with-joker
+  "Returns 1 if hand1 is stronger, -1 if hand2 is stronger, and 0 if they are equal or the hands are incomparable.
+   
+   First-ordering rule: hand type strength.
+   Second-ordering rule: compare hands sequentially.
+   
+   It implements java.util.Comparator.compare."
+  [h1 h2]
+  (let [res-first-order (compare-hands-first-order (:type h1) (:type h2))]
+    (if (zero? res-first-order)
+      (compare-hands-second-order-with-joker (:hand h1) (:hand h2))
+      res-first-order)))
+
+(defn rank-hands 
+  "Ranks the hands from strongest to weakest." 
+  [hands & joker]
+  (println joker)
+  (if joker
+    (sort compare-hands-with-joker hands)
+    (sort compare-hands hands)))
+
+
+(defn bid-to-winnings 
+  "Computes the winnings for a ranked list of bids." 
+  [ranked-bids] 
+  (reduce + (map-indexed (fn [idx val] (* (+ idx 1) val)) ranked-bids)))
+
+(defn winnings
+  "Computes the winnings for a ranked list of hands."
+  [ranked-hands]
+  (bid-to-winnings (map #(:bid %) ranked-hands)))
+
+
+;; ==================
 ;; Parsing logic
-(defn parse-line [line]
-    (->> (clojure.string/split line #"\s")
-         (filter #(not (clojure.string/blank? %)))))
+;; ==================
+(defn parse-hand 
+  "Parses a line into a hand and a bid."
+  [line builder]
+  (if (clojure.string/blank? line) nil
+      (let [vals (clojure.string/split line #"\s")
+            hand (first vals)
+            bid (Integer. (second vals))]
+        {:hand hand :bid bid :type (get-hand-type hand builder)})))
 
-(defn parse-hand [line]
-  (let [vals (parse-line line)
-        hand (first vals)
-        bid (Integer. (second vals))] 
-    {:hand hand :bid bid}))
+(defn parse-input 
+  "Parses the input into a list of hands."
+  [input]
+  (map #(parse-hand % (get-builder false)) (clojure.string/split-lines input)))
 
-(defn parse-input [input]
-  (map parse-hand (clojure.string/split-lines input)))
+(defn parse-input-with-joker
+  "Parses the input into a list of hands."
+  [input]
+  (map #(parse-hand % (get-builder true)) (clojure.string/split-lines input)))
 
-(defn accumulate-bids [acc bid] 
-    (assoc acc bid (+ (acc bid) 1)))
+;; ==================
+;; Main
+;; ==================
+
+(defn solve1 [input]
+  (let [hands (parse-input input)]
+    (winnings (rank-hands hands))))
 
 (defn solve2 [input]
-    (let [hands (parse-input input)
-            sorted-hands (sort sort-hands hands)]
-      (reduce + (map-indexed (fn [idx val] (* (+ idx 1) val)) (map #(:bid %) sorted-hands)))))
+  (let [hands (parse-input-with-joker input)]
+    (winnings (rank-hands hands true))))
 
 (defn main
     "Main entry point."
@@ -85,5 +212,5 @@
     (if (empty? path)
         (println "Usage: day07.clj <input-file>") 
         (do
-          (println "Part 1:" (solve2 (slurp path)))
+          (println "Part 1:" (solve1 (slurp path)))
           (println "Part 2:" (solve2 (slurp path))))))
--- test/aoc/2023/day07_test.clj
+++ test/aoc/2023/day07_test.clj
@@ -1,7 +1,136 @@
 (ns aoc.2023.day07-test
   (:require [clojure.test :refer :all]
-            [aoc.core :refer :all]))
+            [aoc.2023.day07 :as camel-cards :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest card-to-strength-test
+  (testing "Order of card strength"
+    (is (> (:A camel-cards/card-to-strength)
+           (:K camel-cards/card-to-strength)
+           (:Q camel-cards/card-to-strength)
+           (:J camel-cards/card-to-strength)
+           (:T camel-cards/card-to-strength)
+           (:9 camel-cards/card-to-strength)
+           (:8 camel-cards/card-to-strength)
+           (:7 camel-cards/card-to-strength)
+           (:6 camel-cards/card-to-strength)
+           (:5 camel-cards/card-to-strength)
+           (:4 camel-cards/card-to-strength)
+           (:3 camel-cards/card-to-strength)
+           (:2 camel-cards/card-to-strength)))))
+
+(deftest card-to-strength-test
+  (testing "Order of card strength with joker"
+    (is (> (:A camel-cards/card-to-strength-with-joker)
+           (:K camel-cards/card-to-strength-with-joker)
+           (:Q camel-cards/card-to-strength-with-joker) 
+           (:T camel-cards/card-to-strength-with-joker)
+           (:9 camel-cards/card-to-strength-with-joker)
+           (:8 camel-cards/card-to-strength-with-joker)
+           (:7 camel-cards/card-to-strength-with-joker)
+           (:6 camel-cards/card-to-strength-with-joker)
+           (:5 camel-cards/card-to-strength-with-joker)
+           (:4 camel-cards/card-to-strength-with-joker)
+           (:3 camel-cards/card-to-strength-with-joker)
+           (:2 camel-cards/card-to-strength-with-joker)
+           (:J camel-cards/card-to-strength-with-joker)))))
+
+(deftest hand-type-to-strength-test
+  (testing "Order of Hand Type strength"
+    (is (> (:five-of-a-kind camel-cards/hand-type-to-strength)
+           (:four-of-a-kind camel-cards/hand-type-to-strength)
+           (:full-house camel-cards/hand-type-to-strength)
+           (:three-of-a-kind camel-cards/hand-type-to-strength)
+           (:two-pair camel-cards/hand-type-to-strength)
+           (:one-pair camel-cards/hand-type-to-strength)
+           (:high-card camel-cards/hand-type-to-strength)))))
+
+(deftest compare-cards-char-by-char-test
+  (testing "Compare cards by strength"
+    (is (= 1 (camel-cards/compare-cards-char-by-char \A \K))) 
+    (is (= 0 (camel-cards/compare-cards-char-by-char \A \A)))
+    (is (= -1 (camel-cards/compare-cards-char-by-char \K \A)))))
+
+(deftest compare-hands-first-order-test
+  (testing "first ordering rule"
+    (is (= 1 (camel-cards/compare-hands-first-order :two-pair :one-pair)))
+    (is (= 0 (camel-cards/compare-hands-first-order :two-pair :two-pair))))
+    (is (= 1 (camel-cards/compare-hands-first-order :three-of-a-kind :two-pair)))
+    (is (= 0 (camel-cards/compare-hands-first-order :three-of-a-kind :three-of-a-kind))))
+
+(deftest compare-hands-second-order-test
+  (testing "second ordering rule" 
+    (is (= 1 (camel-cards/compare-hands-second-order "33332" "2AAAA")))
+    (is (= 1 (camel-cards/compare-hands-second-order "77888" "77788"))))
+    (is (= 1 (camel-cards/compare-hands-second-order "KK677" "KTJJT")))
+    (is (= -1 (camel-cards/compare-hands-second-order "T55J5" "QQQJA"))))
+
+(deftest compare-hands-test
+  (testing "fallback to second ordering rule for hands of same type"
+    (is (= 1 (camel-cards/compare-hands {:hand "33332" :type :four-of-a-kind} {:hand "2AAAA" :type :four-of-a-kind})))
+    (is (= 1 (camel-cards/compare-hands {:hand "77888" :type :three-of-a-kind} {:hand "77788" :type :three-of-a-kind})))))
+
+(deftest get-hand-type-test
+  (testing "get-hand-type without joker"
+    (let [builder (camel-cards/get-builder false)]
+      (is (= :one-pair (camel-cards/get-hand-type "32T3K" builder)))
+      (is (= :two-pair (camel-cards/get-hand-type "KK677" builder)))
+      (is (= :two-pair (camel-cards/get-hand-type "KTJJT" builder)))
+      (is (= :three-of-a-kind (camel-cards/get-hand-type "T55J5" builder)))
+      (is (= :three-of-a-kind (camel-cards/get-hand-type "QQQJA" builder))))))
+
+(deftest bid-to-winnings-test
+      (testing "Winnings computation for example input"
+        (is (= 6440 (camel-cards/bid-to-winnings [765 220 28 684 483]))) 
+        ))
+
+(deftest winnings-test
+   (testing "Winnings computation for example hands"
+     (is (= 6440 (camel-cards/winnings [{:hand "32T3K" :bid 765}
+                                        {:hand "KTJJT" :bid 220}
+                                        {:hand "KK677" :bid 28}
+                                        {:hand "T55J5" :bid 684}
+                                        {:hand "QQQJA" :bid 483}])))) 
+  (testing "Winnings with joker"
+    (is (= 5905 (camel-cards/winnings [{:hand "32T3K" :bid 765 :type :one-pair}
+                                       {:hand "KK677" :bid 28 :type :two-pair}
+                                       {:hand "T55J5" :bid 684 :type :four-of-a-kind}
+                                       {:hand "QQQJA" :bid 483 :type :four-of-a-kind}
+                                       {:hand "KTJJT" :bid 220 :type :four-of-a-kind}])))))
+
+(deftest rank-hands-test
+  (testing "Rank hands from example"
+    (is (= [{:hand "32T3K" :bid 765 :type :one-pair}
+            {:hand "KTJJT" :bid 220 :type :two-pair}
+            {:hand "KK677" :bid 28 :type :two-pair}
+            {:hand "T55J5" :bid 684 :type :three-of-a-kind}
+            {:hand "QQQJA" :bid 483 :type :three-of-a-kind}] (camel-cards/rank-hands [{:hand "32T3K" :bid 765 :type :one-pair}
+                                                               {:hand "T55J5" :bid 684 :type :three-of-a-kind}
+                                                               {:hand "KK677" :bid 28 :type :two-pair}
+                                                               {:hand "KTJJT" :bid 220 :type :two-pair}
+                                                               {:hand "QQQJA" :bid 483 :type :three-of-a-kind}])))))
+
+
+(deftest parse-hand-test
+  (testing "Parse hand without joker"
+    (let [builder (camel-cards/get-builder false)]
+    (is (= {:hand "32T3K" :bid 765 :type :one-pair} (camel-cards/parse-hand "32T3K 765" builder)))
+    (is (= {:hand "T55J5" :bid 684 :type :three-of-a-kind } (camel-cards/parse-hand "T55J5 684" builder)))
+    (is (= {:hand "KK677" :bid 28 :type :two-pair} (camel-cards/parse-hand "KK677 28" builder)))
+    (is (= {:hand "KTJJT" :bid 220 :type :two-pair} (camel-cards/parse-hand "KTJJT 220" builder)))
+    (is (= {:hand "QQQJA" :bid 483 :type :three-of-a-kind} (camel-cards/parse-hand "QQQJA 483" builder))))))
+
+(deftest parse-input-test
+  (testing "Parse input"
+    (is (= [{:hand "32T3K" :bid 765 :type :one-pair}
+            {:hand "T55J5" :bid 684 :type :three-of-a-kind}
+            {:hand "KK677" :bid 28 :type :two-pair}
+            {:hand "KTJJT" :bid 220 :type :two-pair}
+            {:hand "QQQJA" :bid 483 :type :three-of-a-kind}] (camel-cards/parse-input "32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483")))))
+
+(deftest parse-input-with-joker-test
+  (testing "Parse input with Joker"
+    (is (= [{:hand "32T3K" :bid 765 :type :one-pair} 
+            {:hand "T55J5" :bid 684 :type :four-of-a-kind}
+            {:hand "KK677" :bid 28 :type :two-pair}
+            {:hand "KTJJT" :bid 220 :type :four-of-a-kind}
+            {:hand "QQQJA" :bid 483 :type :four-of-a-kind}] (camel-cards/parse-input-with-joker "32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483")))))
