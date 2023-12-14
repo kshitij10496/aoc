e807b5a492a0d808d1f7e85c0461df64dba71e8f
Kshitij Saraogi <kshitij.saraogi@gmail.com>
2023-12-14 18:49:42 +0100

    feat(day14): solve part 1

--- src/aoc/2023/day14.clj
+++ src/aoc/2023/day14.clj
@@ -1,44 +1,84 @@
 (ns aoc.2023.day14
    (:require [clojure.string :as clojure.string]))
 
+(def foo "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....")
+
 (defn count-rocks
+  [row] 
+  (get (frequencies row) \O 0))
+
+(defn count-rocks-row
   [row]
-  (get (frequencies row) \0))
+  ;; (println row (count (filter #(= % "O") row)))
+  (count (filter #(= % "O") row))
+  )
+
+(defn count-cube-rocks
+  [row]
+  (get (frequencies row) \# 0))
 
 (defn count-space
   [row]
-  (get (frequencies row) \.))
+  (get (frequencies row) \. 0))
 
 (defn tilt-segment
   [segment]
   (let [count-of-rocks (count-rocks segment)
+        count-of-cube-rocks (count-cube-rocks segment)
         count-of-spaces (count-space segment)
-        rocks (clojure.string/join "" (repeat count-of-rocks "0"))
+        rocks (clojure.string/join "" (repeat count-of-rocks "O"))
         spaces (clojure.string/join "" (repeat count-of-spaces "."))]
-    (clojure.string/join "" [rocks spaces])))
+    (if (> count-of-cube-rocks 0) segment
+     (clojure.string/join "" [rocks spaces]))))
 
 (defn segments
   [row]
-  (clojure.string/join "#" (->> (clojure.string/split row #"#")
-       (map tilt-segment))))
+  (->> (partition-by #(= "#" %) row)
+        (map #(clojure.string/join "" %))
+        (map tilt-segment)
+        (map #(clojure.string/split % #""))
+        (reduce concat)))
 
 (defn counter
   [rows]
-  (->> (map-indexed (fn [i row] (* (+ i 1) (count-rocks row))) rows)
+  (->> (map-indexed (fn [i row] (* (+ i 1) (count-rocks-row row))) rows) 
        (reduce +)))
 
 (defn transpose-space
   [space]
   (apply mapv vector space))
 
+(defn build-space
+  [input]
+  (->> input
+       (filter #(not (clojure.string/blank? %)))
+       (map #(clojure.string/split % #""))))
+
+(defn build-row
+  [row]
+  (clojure.string/join "" row))
+
 (defn solve1
   [input]
-  (let [space (build-space input)]
-    (->> (transpose-space space)
-         (map segments)
-         )
-    )
-  )
+  (let [space (build-space (clojure.string/split-lines input))]
+    (counter (reverse (transpose-space (->> (transpose-space space) 
+         (map #(segments %))
+        ;;  (println)
+         ))))))
+
+;; (defn bar 
+;;   [input]
+;;   (reflector/transpose-space (reflector/build-space (reflector/solve1 input)))
+;;   )
 
 (defn main
   [path]
--- test/aoc/2023/day14_test.clj
+++ test/aoc/2023/day14_test.clj
@@ -5,17 +5,50 @@
 
 (deftest count-rocks-test
       (testing "Context of the test assertions"
-        (is (= 6 (reflectors/count-rocks "..00.#0..0.0#..0")))
-        (is (= 2 (reflectors/count-rocks "..00.")))
+        (is (= 5 (reflectors/count-rocks "..O.#O..O.O#..O")))
+        (is (= 1 (reflectors/count-rocks "..O.")))
+        (is (= 0 (reflectors/count-rocks ".....")))
         ))
 
 (deftest tilt-segment-test
       (testing "Context of the test assertions"
-        (is (= "00..." (reflectors/tilt-segment "..00.")))
-        (is (= "000..." (reflectors/tilt-segment "0..0.0")))
-        (is (= "0.." (reflectors/tilt-segment "..0"))))) 
+        (is (= "OO..." (reflectors/tilt-segment "..OO.")))
+        (is (= "OOO..." (reflectors/tilt-segment "O..O.O")))
+        (is (= "OOO..." (reflectors/tilt-segment "O..O.O")))
+        (is (= "O.." (reflectors/tilt-segment "..O"))))) 
+
 
 (deftest segments-test
       (testing "Context of the test assertions"
-        (is (= "00...#000...#0.." (reflectors/segments "..00.#0..0.0#..0")))))
+        (is (= (clojure.string/split "O...#OOO...#O.." #"") (reflectors/segments (clojure.string/split "..O.#O..O.O#..O" #""))))
+        (is (= (clojure.string/split "OOOO....##" #"") (reflectors/segments (clojure.string/split "OO.O.O..##" #""))))))
 
+((deftest build-space-test
+      (testing "Context of the test assertions"
+        (is (= [["O" "." "." "." "." "#" "." "." "." "."] 
+                ["O" "." "O" "O" "#" "." "." "." "." "#"] 
+                ["." "." "." "." "." "#" "#" "." "." "."] 
+                ["O" "O" "." "#" "O" "." "." "." "." "O"] 
+                ["." "O" "." "." "." "." "." "O" "#" "."] 
+                ["O" "." "#" "." "." "O" "." "#" "." "#"] 
+                ["." "." "O" "." "." "#" "O" "." "." "O"] 
+                ["." "." "." "." "." "." "." "O" "." "."] 
+                ["#" "." "." "." "." "#" "#" "#" "." "."] 
+                ["#" "O" "O" "." "." "#" "." "." "." "."]] (reflectors/build-space (clojure.string/split-lines reflectors/foo))))) ))
+
+((deftest transpose-space-test
+   (testing "Context of the test assertions"
+     (is (= [[\O \O \. \O \. \O \. \. \# \#] 
+             [\. \. \. \O \O \. \. \. \. \O] 
+             [\. \O \. \. \. \# \O \. \. \O] 
+             [\. \O \. \# \. \. \. \. \. \.] 
+             [\. \# \. \O \. \. \. \. \. \.] 
+             [\# \. \# \. \. \O \# \. \# \#] 
+             [\. \. \# \. \. \. \O \. \# \.] 
+             [\. \. \. \. \O \# \. \O \# \.] 
+             [\. \. \. \. \# \. \. \. \. \.] 
+             [\. \# \. \O \. \# \O \. \. \.]] (reflectors/transpose-space (clojure.string/split-lines reflectors/foo)))))))
+
+((deftest build-row-test
+      (testing "Context of the test assertions"
+        (is (= "OO.O.O..##" (reflectors/build-row [\O \O \. \O \. \O \. \. \# \#]))))))
