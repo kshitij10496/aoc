9c8d33be252652f02d011273e0bcb38c327bf868
Kshitij Saraogi <kshitij.saraogi@gmail.com>
2025-12-02 14:43:49 +0100

    patch: reorganize

--- 2025/cmd/day-02.kt
+++ 2025/cmd/day-02.kt
@@ -26,10 +26,35 @@ fun isInvalidID(n: Long): Boolean {
     return firstHalf == secondHalf
 }
 
-fun invalidIDs(range: Range): List<Long> {
+// Function to check if an ID is invalid.
+// An ID is invalid if it is made only of some sequence of digits repeated atleast twice.
+// Ex: 55 (5 twice), 6464 (64 twice), 123123 (123 twice) are invalid.
+// Ex: 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
+fun isInvalidIDGeneric(n: Long): Boolean {
+    val idStr = n.toString()
+    val len = idStr.length
+
+    // Check for all possible lengths of the repeating unit with length from 1 to len/2.
+    // Each repeating unit must be at most half the length of the ID.
+    for (unitLen in 1..(len / 2)) {
+        // Complete repeating units can only be formed when the length of the ID is divisible by the unit length.
+        // Alternate: We can consider factorization of len to get possible unit lengths.
+        if (len % unitLen == 0) {
+            val unit = idStr.substring(0, unitLen)
+            val repeated = unit.repeat(len / unitLen)
+            if (repeated == idStr) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+// Function to get all invalid IDs in a given range using the specified invalidation function.
+fun invalidIDs(range: Range, invalidator: (Long) -> Boolean): List<Long> {
     val invalids = mutableListOf<Long>()
     for (id in range.start..range.end) {
-        if (isInvalidID(id)) {
+        if (invalidator(id)) {
             invalids.add(id)
         }
     }
@@ -49,8 +74,15 @@ fun main(args: Array<String>) {
     // Build a list of all invalid IDs across all ranges.
     val invalidIDs = mutableListOf<Long>()
     for (range in ranges) {
-        invalidIDs.addAll(invalidIDs(range))
+        invalidIDs.addAll(invalidIDs(range, ::isInvalidID))
     }
 
     println("Part 1: ${invalidIDs.sum()}")
+
+    // Build a list of all invalid IDs across all ranges.
+    val invalidIDsPart2 = mutableListOf<Long>()
+    for (range in ranges) {
+        invalidIDsPart2.addAll(invalidIDs(range, ::isInvalidIDGeneric))
+    }
+    println("Part 2: ${invalidIDsPart2.sum()}")
 }
